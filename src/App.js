281924a9dbea2e5596ae3617fdcdf7068eb5d20b
Aman Singh <2k22.csiot.2212425@gmail.com>
2024-05-20 17:45:56 +0530

    aman

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import About from './component/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>this is cloud notebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/users" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
